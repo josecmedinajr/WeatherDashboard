5d170a2fce09aff3464c41d22b388e2833aa8a89
Jose Medina <yanoya10@hotmail.com>
2020-02-26 14:40:05 -0500

    API works and pulls data from Miami now. Future update will have input box to populate weather of the user's choice

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ var update = function() {
 }
 setInterval(update, 1000);
 
-// API Key from  https://openweathermap.org/: 3cdcca706cc22e516be1f657eb0929a4, miami call
-//api.openweathermap.org/data/2.5/weather?q=miami&appid=3cdcca706cc22e516be1f657eb0929a4;
+// JQuery getJSON function to get data from openweathermap.org (Miami) with my API: 3cdcca706cc22e516be1f657eb0929a4
+var city = "Miami"
+
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=3cdcca706cc22e516be1f657eb0929a4",function(data){
+    console.log(data);
+
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var temperature = Math.floor(data.main.temp);
+    var weatherCondition = data.weather[0].main
+
+    $(".weather-icon").attr("src", icon);
+    $(".temperature").append(temperature);
+    $(".weather-condition").append(weatherCondition);
+});
 
