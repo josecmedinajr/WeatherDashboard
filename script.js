bc03fab43c4f66c71fab9b7e6c835238bd20f808
Jose Medina <yanoya10@hotmail.com>
2020-02-25 22:49:50 -0500

    updated name of index.html to lower case

--- script.js
+++ script.js
@@ -7,3 +7,4 @@ setInterval(update, 1000);
 
 // API Key from  https://openweathermap.org/: 3cdcca706cc22e516be1f657eb0929a4, miami call
 //api.openweathermap.org/data/2.5/weather?q=miami&appid=3cdcca706cc22e516be1f657eb0929a4;
+
