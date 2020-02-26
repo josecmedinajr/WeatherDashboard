6f91aa03f0218092006256c8ee353d3e20429415
Jose Medina <yanoya10@hotmail.com>
2020-02-25 21:28:46 -0500

    Updated index.html styles.css scripts.js, first deployment of page

---
+++ Assets/Scripts/script.js
@@ -0,0 +1,9 @@
+// updates date and time dynamically using the moment.js function
+var update = function() {
+    document.getElementById("datetime")
+    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+}
+setInterval(update, 1000);
+
+// API Key from  https://openweathermap.org/: 3cdcca706cc22e516be1f657eb0929a4, miami call
+//api.openweathermap.org/data/2.5/weather?q=miami&appid=3cdcca706cc22e516be1f657eb0929a4;
